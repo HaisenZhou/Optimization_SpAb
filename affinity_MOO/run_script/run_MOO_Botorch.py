ff9435d4f4e47f167049123b881aada7bfc4b414
Guangqi <wuroy@LAPTOP-2EO6SOB9.localdomain>
2023-04-16 15:46:30 -0400

    fix the bug of SAAS

--- affinity_MOO/run_script/run_MOO_Botorch.py
+++ affinity_MOO/run_script/run_MOO_Botorch.py
@@ -3,12 +3,12 @@ Run the MOO
 '''
 
 
-import MOO_Botorch.MOO_Botorch as MOO
+import MOO_B.MOO_Botorch as MOO_B
 import logging
 import time
 
 if __name__=="__main__": 
     start_time = time.time()
-    MOO.MOO()
+    MOO_B.MOO_Botorch()
     end_time = time.time()
     logging.info(f"Program finished in: {end_time - start_time} seconds")
